696682496560854a559e5441874dd0fc9c428bb6
Melinda Hasselbring <melinda.hasselbring@gmail.com>
2017-08-02 20:51:07 -0700

    recommiting files

--- models/model.js
+++ models/model.js
@@ -6,7 +6,7 @@
 
   // configure sequelize instance
   // be sure to run mysql command to create database instance
-  var password = "andyhemysql123";
+  var password = "root";
   var sequelize = new Sequelize("database_name", "root", password, {
     host: "localhost",
     dialect: "mysql",
